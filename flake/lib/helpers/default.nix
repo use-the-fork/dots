1c68dbbc45e1e3604c2d7846cd2934a73c2aad82
use-the-fork <sincore@gmail.com>
2024-06-14 17:23:44 -0400

    feat(flake/lib/helpers): Add new helper functions for systemd, filesystem, types, and themes
    This commit includes various helper functions to support:
    - Hardening of services in systemd
    - Handling of file types and file systems
    - Type conversions and checks
    - Theme serialization and SCSS compilation

---
+++ flake/lib/helpers/default.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  src,
+}: let
+  inherit (import ../common.nix {inherit lib;}) import';
+
+  systemd = import' ./systemd.nix;
+  fs = import' ./fs.nix {inherit src;};
+  types = import' ./types.nix;
+  themes = import' ./themes.nix;
+  modules = import' ./modules.nix;
+in {
+  inherit (systemd) hardenService;
+  inherit (fs) fs;
+  inherit (types) filterNixFiles importNixFiles boolToNum fetchKeys containsStrings indexOf intListToStringList;
+  inherit (themes) serializeTheme compileSCSS;
+  inherit (modules) mkModule;
+}
---
+++ flake/lib/helpers/fs.nix
@@ -0,0 +1,30 @@
+{src, ...}: {
+  fs = rec {
+    ## Matchers for file kinds. These are often used with `readDir`.
+    ## Example Usage:
+    ## ```nix
+    ## is-file-kind "directory"
+    ## ```
+    ## Result:
+    ## ```nix
+    ## false
+    ## ```
+    #@ String -> Bool
+    is-file-kind = kind: kind == "regular";
+    is-symlink-kind = kind: kind == "symlink";
+    is-directory-kind = kind: kind == "directory";
+    is-unknown-kind = kind: kind == "unknown";
+
+    ## Get a file path relative to the user's flake.
+    ## Example Usage:
+    ## ```nix
+    ## get-file "systems"
+    ## ```
+    ## Result:
+    ## ```nix
+    ## "/user-source/systems"
+    ## ```
+    #@ Path -> Path
+    get-file = path: "${src}/${path}";
+  };
+}
