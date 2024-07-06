621f6c9ba17979574371535af7178a9c0e8b79fc
use-the-fork <sincore@gmail.com>
2024-07-06 11:30:05 -0400

    feat(monitoring.nix): add Prometheus as a service in system monitoring stack

--- modules/options/system/services/monitoring.nix
+++ modules/options/system/services/monitoring.nix
@@ -1,5 +1,10 @@
-{lib, ...}: let
-  inherit (lib) mkEnableOption;
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkService;
+  meta = config.modules.meta;
   #  sys = config.modules.system;
   #  cfg = sys.services;
   # mkEnableOption is the same as mkEnableOption but with the default value being equal to cfg.monitoring.enable
@@ -9,6 +14,14 @@ in {
     # monitoring tools
     monitoring = {
       enable = mkEnableOption "system monitoring stack";
+
+      prometheus = mkService {
+        name = "Prometheus";
+        type = "monitoring";
+        port = 9100;
+        subDomain = "prometheus.${meta.subDomainName}";
+      };
+
       prometheus.enable = mkEnableOption "Prometheus monitoring service";
       grafana.enable = mkEnableOption "Grafana monitoring service";
     };
