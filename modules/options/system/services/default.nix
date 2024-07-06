33176aad1a24e5c76938eaec341d1321c2052f65
use-the-fork <sincore@gmail.com>
2024-07-06 10:24:31 -0400

    feat(services): add new services and database configurations in nix files

--- modules/options/system/services/default.nix
+++ modules/options/system/services/default.nix
@@ -1,7 +1,40 @@
-{
+{lib, ...}: let
+  inherit (lib) mkService;
+in {
   imports = [
+    ./databases.nix
     ./monitoring.nix
     ./networking.nix
     ./misc.nix
   ];
+
+  options.modules.system = {
+    services = {
+      #      nextcloud = mkService {
+      #        name = "Nextcloud";
+      #        type = "cloud storage";
+      #      };
+
+      nginx = mkService {
+        name = "Nginx";
+        type = "webserver";
+      };
+
+      caddy = mkService {
+        name = "Caddy";
+        type = "webserver";
+      };
+
+      #
+      #      elasticsearch = mkService {
+      #        name = "Elasticsearch";
+      #        port = 9200;
+      #      };
+      #
+      #      kanidm = mkService {
+      #        name = "Kanidm";
+      #        port = 8443;
+      #      };
+    };
+  };
 }
