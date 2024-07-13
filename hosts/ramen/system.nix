f5d358df04d83a0890fedf813f6bd630b37e2190
use-the-fork <sincore@gmail.com>
2024-07-13 12:43:39 -0400

    feat(system.nix): add logind service settings to ignore lid switch actions

--- hosts/ramen/system.nix
+++ hosts/ramen/system.nix
@@ -1,9 +1,20 @@
-{self, ...}: {
+{
+  self,
+  lib,
+  ...
+}: {
   config = {
     boot.kernelParams = [
       "i8042.nomux" # Don't check presence of an active multiplexing controller
       "i8042.nopnp" # Don't use ACPIPn<P / PnPBIOS to discover KBD/AUX controllers
     ];
+
+    services = {
+      logind = {
+        lidSwitch = lib.mkForce "ignore";
+        lidSwitchExternalPower = lib.mkForce "ignore";
+      };
+    };
 
     system = {
       stateVersion = "23.11";
