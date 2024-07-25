b29a0991ddb806e2fa5e29f2de5fb2f0d1dbc490
use-the-fork <sincore@gmail.com>
2024-07-25 17:27:08 -0400

    refactor(cpu): streamline hardware configuration files for Intel and AMD

--- modules/core/common/system/hardware/cpu/amd/default.nix
+++ modules/core/common/system/hardware/cpu/amd/default.nix
@@ -4,12 +4,13 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf mkMerge versionOlder versionAtLeast;
-  inherit (dev.cpu.amd) pstate zenpower;
-
+  inherit (lib) isx86Linux;
+  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.strings) versionOlder versionAtLeast;
   dev = config.modules.device;
 
   kver = config.boot.kernelPackages.kernel.version;
+  inherit (dev.cpu.amd) pstate zenpower;
 in {
   config = mkIf (builtins.elem dev.cpu.type ["amd" "vm-amd"]) {
     environment.systemPackages = [pkgs.amdctl];
@@ -17,14 +18,23 @@ in {
     hardware.cpu.amd.updateMicrocode = true;
     boot = mkMerge [
       {
+        # Always load the kvm-amd module for Virtualization
+        # bellow modules allow for Virtualization on AMD cpus
+        # `"iommu=pt"` kernel parameter can be passed to remove
+        # IOMMU overhead
+        kernelModules = ["kvm-amd"];
+        kernelParams = ["amd_iommu=on"];
+      }
+
+      (mkIf (isx86Linux pkgs) {
         kernelModules = [
-          "kvm-amd" # amd virtualization
           "amd-pstate" # load pstate module in case the device has a newer gpu
           "zenpower" # zenpower is for reading cpu info, i.e voltage
           "msr" # x86 CPU MSR access device
         ];
+
         extraModulePackages = [config.boot.kernelPackages.zenpower];
-      }
+      })
 
       (mkIf (pstate.enable && (versionAtLeast kver "5.17") && (versionOlder kver "6.1")) {
         kernelParams = ["initcall_blacklist=acpi_cpufreq_init"];
@@ -35,8 +45,9 @@ in {
         kernelParams = ["amd_pstate=passive"];
       })
 
-      # for older kernels
-      # see <https://github.com/NixOS/nixos-hardware/blob/c256df331235ce369fdd49c00989fdaa95942934/common/cpu/amd/pstate.nix>
+      # For older kernels.
+      # See:
+      #  <https://github.com/NixOS/nixos-hardware/blob/c256df331235ce369fdd49c00989fdaa95942934/common/cpu/amd/pstate.nix>
       (mkIf (pstate.enable && (versionAtLeast kver "6.3")) {
         kernelParams = ["amd_pstate=active"];
       })
