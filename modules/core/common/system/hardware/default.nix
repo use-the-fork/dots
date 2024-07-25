9efe58c102467e277af2121e6d9356b7c5a18921
use-the-fork <sincore@gmail.com>
2024-07-25 17:43:42 -0400

    feat(hardware): enable TPM2 and Bluetooth support
    - Enable Trusted Platform Module 2 (TPM2) with associated tools and daemons.
    - Add new module for Bluetooth configuration and enablement.
    - Rename generic.nix to redistributable.nix for clarity.

--- modules/core/common/system/hardware/default.nix
+++ modules/core/common/system/hardware/default.nix
@@ -1,11 +1,12 @@
 {
   imports = [
-    ./cpu
-    ./gpu
-    ./multimedia
+    ./cpu # cpu specific options
+    ./gpu # gpu specific options
+    ./multimedia # enable multimedia: e.g. sound and video
 
-    ./generic.nix
-    ./tpm.nix
-    ./yubikey.nix
+    ./bluetooth.nix # bluetooth and device management
+    ./tpm.nix # trusted platform module
+    ./yubikey.nix # yubikey device support and management tools
+    ./redistributable.nix # Non-free redstributable software
   ];
 }
