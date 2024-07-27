6f85cf29891b08208d02c7dbdfa8ecc1ad2d7c72
use-the-fork <sincore@gmail.com>
2024-07-27 10:07:53 -0400

    fix(boot): restore plymouth theme configuration and add a temporary fallback

--- modules/core/common/system/os/boot/plymouth.nix
+++ modules/core/common/system/os/boot/plymouth.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   pkgs,
-  self',
   ...
 }: let
   inherit (pkgs) plymouth;
@@ -13,18 +12,26 @@ in {
   config = mkIf cfg.enable {
     # configure plymouth theme
     # <https://github.com/adi1090x/plymouth-themes>
-    boot.plymouth = let
-      pack = cfg.pack;
-      theme = cfg.theme;
-    in
-      {
-        enable = true;
-      }
-      // lib.optionalAttrs cfg.withThemes {
-        themePackages = [(self'.packages.plymouth-themes.override {inherit pack theme;})];
+    #    boot.plymouth = let
+    #      pack = cfg.pack;
+    #      theme = cfg.theme;
+    #    in
+    #      {
+    #        enable = true;
+    #      }
+    #      // lib.optionalAttrs cfg.withThemes {
+    #        themePackages = [(self'.packages.plymouth-themes.override {inherit pack theme;})];
+    #
+    #        inherit theme;
+    #      };
 
-        inherit theme;
-      };
+    #FIXME: Not sure why theme not working.
+    boot.plymouth = {
+      enable = true;
+      #        font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+      themePackages = [pkgs.nixos-bgrt-plymouth];
+      theme = "nixos-bgrt";
+    };
 
     # make plymouth work with sleep
     powerManagement = {
